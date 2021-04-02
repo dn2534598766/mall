524bc3de7f5e5ec3b8109b056dce23e5952c56e1
dn2534598766 <2534598766@qq.com>
2021-04-02 23:18:12 +0800

    commit

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,20 +5,34 @@ const app = getApp()
 
 Page({
   data: {
-   banner:[]
+   banner:[],
+   commodity:[]
   },
 
   onLoad: function() {
     let that=this
+    
     db.collection('swiper').get({
       success(res){
-        console.log('获取云数据库成功',res)
+        console.log('获取swiper云数据库成功',res)
         that.setData({
           banner:res.data
         })
       },
       err(res){
-        console.log('获取云数据库失败',res)
+        console.log('获取swiper云数据库失败',res)
+      }
+    }),
+    db.collection('commodity').get({
+      success(res){
+        console.log('获取commodity云数据库成功',res)
+        that.setData({
+          commodity:res.data
+        })
+        
+      },
+      err(res){
+        console.log('获取commodity云数据库失败',res)
       }
     })
   },
